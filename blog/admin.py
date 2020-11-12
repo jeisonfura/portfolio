9b6b7b0e30118433cfc45103796c887f1bd228ff
PacketDevil <jeisonfura@gmail.com>
2020-11-12 11:45:11 -0800

    removing tinymce from admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,3 @@ from django.contrib import admin
 from .models import Blog, models
 
 admin.site.register(Blog)
-
-formfield_overrides = {
-    models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 30})},
-}
